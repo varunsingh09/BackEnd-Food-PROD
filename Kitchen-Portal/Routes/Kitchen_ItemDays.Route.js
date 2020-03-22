0a21a1f22988ade5976c8dcf37f4b4ea1d52e5db
Varun Singh <varun.singh@homeshop18.com>
2020-03-22 22:17:17 +0530

    working on zipcodes

--- Kitchen-Portal/Routes/Kitchen_ItemDays.Route.js
+++ Kitchen-Portal/Routes/Kitchen_ItemDays.Route.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 var path = require('path');
 const { KitchenItemServingDays } = require('./../../Kitchen-Portal/Models/Kitchen_ItemDays.Model');
-const { serving_zipcodes, } = require('./../../middleware')
 //const { MasterAdminSignupSchema } = require('./../Models/Master_SignupSignin.Model');
 const { ProductSchema } = require('./../../MasterAdmin-Portal/Models/MProduct.model');
-const { ZipcodeKitchensSchema } = require('./../../MasterAdmin-Portal/Models/MZipcode-Kitchen.model');
-
 const { CaptureErrorsSchema } = require('./../../Common-Model-Routes/Models/Error.model');
 
 // This API is to store Kitchen product serving days 
@@ -47,7 +44,6 @@ router.post('/ItemServingDays', async (req, res, next) => {
       next(error);
     }
   }
-
 
 
   try {
@@ -100,7 +96,7 @@ router.post('/ItemServingDays', async (req, res, next) => {
     let servingdaysRsponse = await servingdays.save();
 
     //console.log("--->>",servingdaysRsponse)
-    return res.status(200).json({ success: [{ 'msg': 'Serving days save successfully.' }] });
+    return res.status(200).json({ success: [{ 'msg': 'Serving days save successfully.', "data": servingdaysRsponse }] });
 
   } catch (error) {
 
@@ -130,83 +126,6 @@ router.post('/ItemServingDays', async (req, res, next) => {
 })
 
 
-//check serving zipcode
-
-
-router.post('/ItemServingZipCodes', async (req, res, next) => {
-
-
-  let zipcodes = typeof req.body.zipcodes === "string" ? [req.body.zipcodes] : req.body.zipcodes
-
-  let requestedZipCodes = zipcodes.map(Number)
-
-  //console.log(serving_zipcodes, "<<====>>", requestedZipCodes)
-  let notExistZipcodes = requestedZipCodes.filter(x => !serving_zipcodes.includes(x));
-
-  //console.log("find elements", intersection)
-  if (notExistZipcodes.length > 0) {
-    return res.status(200).json({ data: [{ 'error': 'We are not serving these zipcodes', "notserving_zipcodes": notExistZipcodes, 'success': 'We are serving these zipcodes', "serving_zipcodes": serving_zipcodes }] });
-  }
-
-  let kitchen_names = typeof req.body.kitchen_names === "object" ? req.body.kitchen_names.map(String) : req.body.kitchen_names
-
-  //console.log("---------------", kitchen_names)
-  let product = await ProductSchema.count({ kitchen_name: kitchen_names, u_id: req.body.u_id });
-
-  if (product === 0) {
-    return res.status(200).json({ errors: [{ 'msg': 'This Kitchen dose not exit' }] });
-
-  }
-
-  let zipcodesData = await ZipcodeKitchensSchema.find({ zipcodes: zipcodes, kitchen_names: kitchen_names });
-
-  //console.log("---------------", zipcodesData.length===0)
-  if (zipcodesData.length === 0) {
-
-    try {
-
-      zipcodesDataArray = new ZipcodeKitchensSchema({
-        kitchen_names: kitchen_names,
-        zipcodes: zipcodes,
-        state: 'chicago',
-      });
-
-      let zipcodesDataResponse = await zipcodesDataArray.save();
-
-      //console.log("--->>",servingdaysRsponse)
-      return res.status(200).json({ success: [{ 'success': 'Data saved successfully', "data": zipcodesDataResponse }] });
-
-    } catch (error) {
-
-      let str = `E11000 duplicate key error collection: test.ZipcodeKitchensSchema index`
-
-      if (error.code !== 11000) {
-
-        errs = new CaptureErrorsSchema({
-          error: error,
-          errorRoute: 'ZipcodeKitchensSchema',
-          kitchen_name: kitchen_names
-
-
-        });
-
-
-        await errs.save();
-
-        if (error.name === 'MongoError' && error.code === 11000) {
-          return res.status(200).json({ errors: { 'msg': [error.errmsg.replace(str, `There was a duplicate key error in`).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1', ' :')] } });
-        } else {
-          next(error);
-        }
-      }
-    }
-
-  } else {
-    return res.status(200).json({ errors: [{ 'success': 'These kitchen already serving zipcodes', "serving_zipcodes": zipcodesData }] });
-
-  }
-
-})
 
 
 
--- MasterAdmin-Portal/Routes/MZipcode-Kitchen.Route.js
+++ MasterAdmin-Portal/Routes/MZipcode-Kitchen.Route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { ZipcodeKitchensSchema } = require('./../../MasterAdmin-Portal/Models/MZipcode-Kitchen.model')
-const { KitchenSignupSchema} = require('./../../Kitchen-Portal/Models/KSignup-model');
-const { serving_zipcodes} = require('./../../middleware')
-const {CaptureErrorsSchema} = require('./../../Common-Model-Routes/Models/Error.model')
+const { ProductSchema } = require('./../../MasterAdmin-Portal/Models/MProduct.model');
+const { serving_zipcodes, } = require('./../../middleware')
+const { CaptureErrorsSchema } = require('./../../Common-Model-Routes/Models/Error.model')
 
 // Post 
 // Below Route is master admin to enter zipcode and kitchens ,this will tell which kitchen is serving which zipcodes 
@@ -17,89 +17,86 @@ const {CaptureErrorsSchema} = require('./../../Common-Model-Routes/Models/Error.
 // Not completed -work in progress
 
 
+router.post('/ZipcodeKitchens', async (req, res, next) => {
 
-router.post('/ZipcodeKitchens', async function (req, res, next) {
+    let email = "sayed@gmail.com"
 
-    console.log(serving_zipcodes, req.body.zipcodes)
+    if (email !== req.body.email) {
+        return res.status(200).json({ data: [{ 'error': 'You are not authorize person to access this api.', "email": req.body.email }] });
 
-    // bulk kitchen name validation needed ,mention which kitchen name dosnt exist to user
-    // let masteradmin = await KitchenSignupSchema.findOne({ kitchen_name: req.body.kitchen_name });
-
-   
-    if (masteradmin === null) {
-       
-        return res.status(200).json({ errors: [{ 'msg': 'This Kitchen dosnt exit',param:'kitchen_name' }] });
     }
-  
-
-    // bulk zipcode validation needed ,mention which zipcode dosnt exist to user
 
 
-    // let zipcodes = await serving_zipcodes.findOne({ zipcodes: req.body.zipcodes });
+    let zipcodes = typeof req.body.zipcodes === "string" ? [req.body.zipcodes] : req.body.zipcodes
 
+    let requestedZipCodes = zipcodes.map(Number)
 
-   
-    // if (zipcodes=== null) {
-       
-    //     return res.status(200).json({ errors: [{ 'msg': 'This zipcode dosnt exit',param:'kitchen_name' }] });
-    // }
-  
+    //console.log(serving_zipcodes, "<<====>>", requestedZipCodes)
+    let notExistZipcodes = requestedZipCodes.filter(x => !serving_zipcodes.includes(x));
 
+    //console.log("find elements", intersection)
+    if (notExistZipcodes.length > 0) {
+        return res.status(200).json({ data: [{ 'error': 'We are not serving these zipcodes', "notserving_zipcodes": notExistZipcodes, 'success': 'We are serving these zipcodes', "serving_zipcodes": serving_zipcodes }] });
+    }
 
-    
+    let kitchen_names = typeof req.body.kitchen_names === "object" ? req.body.kitchen_names.map(String) : req.body.kitchen_names
 
-    try {
-        admin = new ZipcodeKitchensSchema({
+    //console.log("---------------", kitchen_names)
+    let product = await ProductSchema.count({ kitchen_name: kitchen_names });
 
-            state: req.body.state,
-            zipcodes: req.body.zipcodes,
-            kitchen_names: req.body.kitchen_names,
-            
-        });
+    if (product === 0) {
+        return res.status(200).json({ errors: [{ 'msg': 'This Kitchen dose not exit' }] });
 
+    }
 
-        await admin.save();
+    let zipcodesData = await ZipcodeKitchensSchema.find({ zipcodes: zipcodes, kitchen_names: kitchen_names });
 
-        
-        return res.status(200).send({ response: admin, 'msg': 'Successfully Added Zipcodes with kitchens' });
+    //console.log("---------------", zipcodesData.length===0)
+    if (zipcodesData.length === 0) {
 
+        try {
 
-       
+            zipcodesDataArray = new ZipcodeKitchensSchema({
+                kitchen_names: kitchen_names,
+                zipcodes: zipcodes,
+                state: 'chicago',
+            });
 
+            let zipcodesDataResponse = await zipcodesDataArray.save();
 
-    } catch (error) {
-        let str = `E11000 duplicate key error collection: test.masteradminsignups index`
+            //console.log("--->>",servingdaysRsponse)
+            return res.status(200).json({ success: [{ 'success': 'Data saved successfully', "data": zipcodesDataResponse }] });
 
-     
-            // This below code will capture error and store it to ERROR collection 
+        } catch (error) {
 
-            if(error.code !==11000){
-                
+            let str = `E11000 duplicate key error collection: test.ZipcodeKitchensSchema index`
+
+            if (error.code !== 11000) {
+
                 errs = new CaptureErrorsSchema({
                     error: error,
-                    errorRoute: 'ZipcodeKitchens',
-                    
-                   
+                    errorRoute: 'ZipcodeKitchensSchema',
+                    kitchen_name: kitchen_names
+
                 });
-        
-    
+
+
                 await errs.save();
-    
-    
-    
+
+                if (error.name === 'MongoError' && error.code === 11000) {
+                    return res.status(200).json({ errors: { 'msg': [error.errmsg.replace(str, `There was a duplicate key error in`).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1', ' :')] } });
+                } else {
+                    next(error);
+                }
             }
-            
-
-
-        if (error.name === 'MongoError' && error.code === 11000) {
-        let ermsg = error.errmsg.replace(str, `Duplicate key `).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1',' :')            
-        console.log(error,"-------",ermsg)   
-        return res.status(200).json({ errors:[{'msg':ermsg}] });
-        } else {
-            next(error);
         }
+
+    } else {
+        return res.status(200).json({ errors: [{ 'success': 'These kitchen already serving zipcodes', "serving_zipcodes": zipcodesData }] });
+
     }
 
-});
+})
+
 
 module.exports = router;
