2f1aaa6347013ab5700bf11746c1038d7414436c
Varun Singh <varun.singh@homeshop18.com>
2020-03-28 20:24:14 +0530

    add new route for filter serving days

--- Kitchen-Portal/Routes/Kitchen_ItemDays.Route.js
+++ Kitchen-Portal/Routes/Kitchen_ItemDays.Route.js
@@ -146,46 +146,27 @@ router.post('/ItemServingDays', async (req, res, next) => {
 
 
 //portal display kitchen name with item
-router.post('/ItemServingDaysMatserAdmin', async (req, res, next) => {
+router.get('/ItemServingDaysMatserAdmin', async (req, res, next) => {
   //console.log("---", typeof req.body.serving_days)
-  let kitchen_name = `chicago kitchen`
 
-  let serving_days = typeof req.body.serving_days === "object" ? req.body.serving_days.map(Number) : [req.body.serving_days[0]].map(Number)
-
-  //console.log("--->>>", serving_days)
-
-  let date = new Date();
-  let day = date.getDay()
-
-  //console.log("---------", serving_days)
-  let currentDay = serving_days.every(function (e) {
-    return e >= day;
-  });
-
-
-  if (currentDay === false) {
-
-    return res.status(200).json({ errors: [{ 'msg': 'Previous days can not be update' }] });
-
-  }
-
-  let serving_days_length = serving_days.length
-
-  let item_type_length = typeof req.body.item_type === "object" ? req.body.item_type.map(String).length : 1
-
-  let item_name_length = typeof req.body.item_name === "object" ? req.body.item_name.map(String).length : 1
-
-  let item_name = item_name_length > 1 ? req.body.item_name[0] : req.body.item_name
-  let item_type = item_type_length > 1 ? req.body.item_type[0] : req.body.item_type
-  
   try {
 
-    let product = await ProductSchema.count({ kitchen_name: kitchen_name, item_name: item_name });
+    //filter data for master admin and kitchen specific
 
-    if (product === 0) {
-      return res.status(200).json({ errors: [{ 'msg': 'This Kitchen or item dose not exit' }] });
-
+    //refine for kitchen specific
+    const match = {}
+    if (req.body.kitchen_name !== undefined) {
+      match.kitchen_name = req.body.kitchen_name
     }
+    // end here
+    
+    let KitchenItemServing = await KitchenItemServingDays.find(match);
+    if (KitchenItemServing.length > 0) {
+      return res.status(200).json({ success: [{ 'data': KitchenItemServing, "total_kitchen": KitchenItemServing.length }] });
+    } else {
+      return res.status(200).json({ errors: [{ 'msg': `No kitechne found with provided kitchen ${req.body.kitchen_name}`, "total_kitchen": KitchenItemServing.length }] });
+    }
+
 
 
   } catch (error) {
@@ -200,78 +181,7 @@ router.post('/ItemServingDaysMatserAdmin', async (req, res, next) => {
   }
 
 
-  try {
 
-    if ((item_type_length > 3 && item_type === "MainCourse") && serving_days_length > 3) {
-
-      //console.log("come")
-
-      return res.status(200).json({ errors: [{ 'msg': 'Only 3 maincourse Item PER DAY from one kitchen.' }] });
-    }
-
-
-    if (item_type === "MainCourse" && serving_days_length > 3) {
-      return res.status(200).json({ errors: [{ 'msg': 'Max 3 Days per week same item can be served by kitchen.' }] });
-    }
-
-    let items = await KitchenItemServingDays.count({ kitchen_name: kitchen_name, item_type: item_type, serving_days: serving_days, item_name: item_name });
-
-    if (items !== 0) {
-      return res.status(200).json({ errors: [{ 'msg': 'Item already serving by this kitchen' }] });
-
-    }
-
-  } catch (error) {
-
-    let str = `E11000 duplicate key error collection: test.itemservingdays index`
-
-    if (error.name === 'MongoError' && error.code === 11000) {
-      return res.status(200).json({ errors: { 'msg': [error.errmsg.replace(str, `There was a duplicate key error in`).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1', ' :')] } });
-    } else {
-      next(error);
-    }
-  }
-
-
-  try {
-
-    servingdays = new KitchenItemServingDays({
-      kitchen_name: kitchen_name,
-      u_id: req.body.u_id,
-      item_type: item_type,
-      item_name: item_name,
-      serving_days: serving_days,
-
-    });
-    let servingdaysRsponse = await servingdays.save();
-
-    //console.log("--->>",servingdaysRsponse)
-    return res.status(200).json({ success: [{ 'msg': 'Serving days save successfully.', "data": servingdaysRsponse }] });
-
-  } catch (error) {
-
-    let str = `E11000 duplicate key error collection: test.itemservingdays index`
-
-    if (error.code !== 11000) {
-
-      errs = new CaptureErrorsSchema({
-        error: error,
-        errorRoute: 'ItemServingDays',
-        kitchen_name: kitchen_name
-
-
-      });
-
-
-      await errs.save();
-
-      if (error.name === 'MongoError' && error.code === 11000) {
-        return res.status(200).json({ errors: { 'msg': [error.errmsg.replace(str, `There was a duplicate key error in`).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1', ' :')] } });
-      } else {
-        next(error);
-      }
-    }
-  }
 
 })
 
