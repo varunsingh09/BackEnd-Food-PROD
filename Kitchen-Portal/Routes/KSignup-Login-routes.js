314967a3082bf8b0dbf814d573f50aa2a060d17c
syed5511 <syedhaq.merndev@gmail.com>
2020-04-24 14:08:50 -0500

    updating new changes pls merge this to master

--- Kitchen-Portal/Routes/KSignup-Login-routes.js
+++ Kitchen-Portal/Routes/KSignup-Login-routes.js
@@ -263,7 +263,7 @@ router.post('/KitchenSignInLogout',async (req, res, next) => {
 
 // Kitchen Login 
 // This API will generate JWT token when on successful sign in 
-// Post - localhost:3001/kitchen/KLogin
+// Post - localhost:3001/kitchen/KitchenLogin
 
 
 
