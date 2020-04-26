511d6d5d4e4f1371e994b8f82a17a7703d1752f8
Varun Singh <varun.singh@homeshop18.com>
2020-04-26 20:03:34 +0530

    change db config

--- Kitchen-Portal/Routes/KSignup-Login-routes.js
+++ Kitchen-Portal/Routes/KSignup-Login-routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { KitchenSignupSchema } = require('./../../Kitchen-Portal/Models/KSignup-model')
 //const { validateMeChecks } = require('./../middleware')
 const { validationResult } = require("express-validator/check");
-const { jwtSignin, jwtVerifyToken, validateMeChecks,CustomerSignInValidations } = require('./../../middleware')
-const {CaptureErrorsSchema} = require('./../../Common-Model-Routes/Models/Error.model')
+const { jwtSignin, jwtVerifyToken, validateMeChecks, CustomerSignInValidations } = require('./../../middleware')
+const { CaptureErrorsSchema } = require('./../../Common-Model-Routes/Models/Error.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const rounds = 10
@@ -27,88 +27,85 @@ router.post('/KitchenSignup', validateMeChecks, async function (req, res, next) 
     }
 
 
-     let ErrMsg=[];
-     let IsalreadyExist=false;
- 
+    let ErrMsg = [];
+    let IsalreadyExist = false;
 
-    let adminK = await KitchenSignupSchema.findOne({kitchen_name:req.body.kitchen_name});
-    if  (adminK!==null)
-    {
-        IsalreadyExist=true;
-        ErrMsg.push( {msg:"Kitchen already exist", param:'kitchen_name'} );
+
+    let adminK = await KitchenSignupSchema.findOne({ kitchen_name: req.body.kitchen_name });
+    if (adminK !== null) {
+        IsalreadyExist = true;
+        ErrMsg.push({ msg: "Kitchen already exist", param: 'kitchen_name' });
         //ErrMsg.Kitchen="Kitchen already exist";
-        
+
     }
- 
-    let adminE = await KitchenSignupSchema.findOne({email:req.body.email});
-    if  (adminE!==null)
-    {
-        IsalreadyExist=true;
-        ErrMsg.push({msg:"Email already exist",param:'email'});
+
+    let adminE = await KitchenSignupSchema.findOne({ email: req.body.email });
+    if (adminE !== null) {
+        IsalreadyExist = true;
+        ErrMsg.push({ msg: "Email already exist", param: 'email' });
         //ErrMsg.Email="Email already exist";
-       
+
     }
-     
-    let adminP = await KitchenSignupSchema.findOne({contact_no:req.body.contact_no});
-    if  (adminP!==null)
-    {
-        IsalreadyExist=true;
-        ErrMsg.push({msg:"Phone already exist",param:'contact_no'});
+
+    let adminP = await KitchenSignupSchema.findOne({ contact_no: req.body.contact_no });
+    if (adminP !== null) {
+        IsalreadyExist = true;
+        ErrMsg.push({ msg: "Phone already exist", param: 'contact_no' });
         //ErrMsg.Phone="Phone already exist";
-       
+
     }
-    
+
 
     if (IsalreadyExist) {
         return res.status(200).json({ errors: ErrMsg });
 
-               // return res.status(200).json({ errors: [{'msg': ErrMsg+ ' already exist in database,please use unique with group'}] });
+        // return res.status(200).json({ errors: [{'msg': ErrMsg+ ' already exist in database,please use unique with group'}] });
 
     } else {
 
 
 
-    let hashPassword = bcrypt.hashSync(req.body.password, rounds);
+        let hashPassword = bcrypt.hashSync(req.body.password, rounds);
 
-    try {
-        admin = new KitchenSignupSchema({
-            kitchen_name: req.body.kitchen_name,
-            address:req.body.address,
-            state: req.body.state,
-            city: req.body.city,
-            zipcode: req.body.zipcode,
-            email: req.body.email,
-            password: hashPassword,
-            contact_no: req.body.contact_no,
-            agreement_policy: req.body.agreement_policy,
-        });
-
-
-        await admin.save();
+        try {
+            admin = new KitchenSignupSchema({
+                kitchen_name: req.body.kitchen_name,
+                address: req.body.address,
+                state: req.body.state,
+                city: req.body.city,
+                zipcode: req.body.zipcode,
+                email: req.body.email,
+                password: hashPassword,
+                contact_no: req.body.contact_no,
+                agreement_policy: req.body.agreement_policy,
+            });
 
 
-       
-        //sending email from 
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'dineout2018@gmail.com',
-                pass: 'dineout@2018'
-            }
-        });
-        let href = `http://localhost:3001/kitchen/verifyEmail/?id=${admin._id}`
-
-        // sending mail to 
-        var mailOptions = {
-            from: 'dineout2018@gmail.com',
-            to: 'Syedhaq5511@gmail.com',// req.body.email
-            subject: 'Sending Email using Node.js test mail',
-           // text: 'That was easy node class Today!'
+            await admin.save();
 
 
-            //html for email
 
-            html: `<!DOCTYPE html>
+            //sending email from 
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'dineout2018@gmail.com',
+                    pass: 'dineout@2018'
+                }
+            });
+            let href = `http://localhost:3001/kitchen/verifyEmail/?id=${admin._id}`
+
+            // sending mail to 
+            var mailOptions = {
+                from: 'dineout2018@gmail.com',
+                to: 'Syedhaq5511@gmail.com',// req.body.email
+                subject: 'Sending Email using Node.js test mail',
+                // text: 'That was easy node class Today!'
+
+
+                //html for email
+
+                html: `<!DOCTYPE html>
                             <html>
                             <head>
                             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -154,65 +151,66 @@ router.post('/KitchenSignup', validateMeChecks, async function (req, res, next) 
                             </body>
                         </html>`
 
-        };
+            };
 
-        //sending email method or function
-        transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-                
-                // this will capture if error comes in email sent 
+            //sending email method or function
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+
+                    // this will capture if error comes in email sent 
                     errs = new CaptureErrorsSchema({
-                    error: error,
-                    errorRoute: 'KitchenSignup ',
-                    errorMethod:'Transporter_email',
-                    email: req.body.email,
-                    kitchen_name:req.body.kitchen_name
-                   
-                   
-                });
-        
-    
-                errs.save();
-
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
+                        error: error,
+                        errorRoute: 'KitchenSignup ',
+                        errorMethod: 'Transporter_email',
+                        email: req.body.email,
+                        kitchen_name: req.body.kitchen_name
 
 
-        return res.status(200).send({ success: admin, 'route': 'https://yahoo.com','Msg': 'Successfully Created Master User' });
+                    });
 
 
-    } catch (error) {
-        let str = `E11000 duplicate key error collection: test.masteradminsignups index`
+                    errs.save();
 
-     
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+
+
+            return res.status(200).send({ success: admin, 'route': 'https://yahoo.com', 'Msg': 'Successfully Created Master User' });
+
+
+        } catch (error) {
+            let str = `E11000 duplicate key error collection: test.masteradminsignups index`
+
+
             // This below code will capture error and store it to ERROR collection 
 
-            if(error.code !==11000){
-                
+            if (error.code !== 11000) {
+
                 errs = new CaptureErrorsSchema({
                     error: error,
                     errorRoute: 'KitchenSignup',
                     email: req.body.email,
-                    kitchen_name:req.body.kitchen_name
-                   
-                   
-                }); 
-    
-                await errs.save();   
-    
-            }
-            
-        if (error.name === 'MongoError' && error.code === 11000) {
-        let ermsg = error.errmsg.replace(str, `Duplicate key `).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1',' :')            
-        return res.status(200).json({ errors:[{'msg':ermsg}] });
-        } else {
-            next(error);
-        }
-    }
+                    kitchen_name: req.body.kitchen_name
 
-}});
+
+                });
+
+                await errs.save();
+
+            }
+
+            if (error.name === 'MongoError' && error.code === 11000) {
+                let ermsg = error.errmsg.replace(str, `Duplicate key `).replace(/[':'",.<>\{\}\[\]\\\/]/gi, "").replace('dup key', '').replace('_1', ' :')
+                return res.status(200).json({ errors: [{ 'msg': ermsg }] });
+            } else {
+                next(error);
+            }
+        }
+
+    }
+});
 
 
 
@@ -223,17 +221,17 @@ router.post('/KitchenSignup', validateMeChecks, async function (req, res, next) 
 // Post -localhost:3001/kitchen/KitchenEmailVerify
 
 
-router.post('/KitchenEmailVerify',async (req, res, next) => {
-console.log("=========",req.body,"-----")
+router.post('/KitchenEmailVerify', async (req, res, next) => {
+    console.log("=========", req.body, "-----")
 
     // Check if this user already exisits
     let admin = await KitchenSignupSchema.findOne({ email: req.body.email, status: true });
     if (admin == null) {
 
-        return res.status(200).json({ errors:[{"msg": 'That admin dose not exisits! Or deactivated, Please check login details' }]});
+        return res.status(200).json({ errors: [{ "msg": 'That admin dose not exisits! Or deactivated, Please check login details' }] });
 
     }
-    
+
 });
 
 
@@ -242,13 +240,13 @@ console.log("=========",req.body,"-----")
 // this code is pending ,incomplete code , this has to delete token from server side 
 // this url you will see at kitchen sign in page 
 
-router.post('/KitchenSignInLogout',async (req, res, next) => {
+router.post('/KitchenSignInLogout', async (req, res, next) => {
 
     //console.log(req.body['x-access-token'],"--------",req.headers)
     //jwt.destroy(req.body['x-access-token'])
 
-    return res.status(200).json({ errors:[{"msg": 'Logout sucessfuly' }]});
-    
+    return res.status(200).json({ errors: [{ "msg": 'Logout sucessfuly' }] });
+
 });
 
 
@@ -259,12 +257,12 @@ router.post('/KitchenSignInLogout',async (req, res, next) => {
 
 
 
-router.post('/KitchenLogin',CustomerSignInValidations,async (req, res, next) => {
+router.post('/KitchenLogin', CustomerSignInValidations, async (req, res, next) => {
 
 
 
     const errors = validationResult(req);
-    
+
     if (!errors.isEmpty()) {
         return res.status(200).json({ errors: errors.array({ onlyFirstError: true }) });
     }
@@ -274,24 +272,24 @@ router.post('/KitchenLogin',CustomerSignInValidations,async (req, res, next) => 
     let admin = await KitchenSignupSchema.findOne({ email: req.body.email, status: true });
     if (admin == null) {
 
-        return res.status(200).json({ errors:[{"msg": 'That admin dose not exisits! Or deactivated, Please contact Administrator' }]});
+        return res.status(200).json({ errors: [{ "msg": 'That admin dose not exisits! Or deactivated, Please contact Administrator' }] });
 
     }
     let compPassword = bcrypt.compareSync(req.body.password, admin.password)
 
     if (compPassword == false) {
 
-        return res.status(200).json({ errors:[{"msg": 'Incorrect Password!! Or  Account deactivated, Please check login details' }]});
+        return res.status(200).json({ errors: [{ "msg": 'Incorrect Password!! Or  Account deactivated, Please check login details' }] });
 
     } else {
 
         let adminId = admin._id
-   
+
         try {
-            let token = jwtSignin(req, res, next, { adminId: adminId,admin:admin })
-           //console.log("----",token)
-          //res.setHeader('Content-Type', 'text/plain');
-          // return res.send({ auth: true, admin,token:token });
+            let token = jwtSignin(req, res, next, { adminId: adminId, admin: admin })
+            //console.log("----",token)
+            //res.setHeader('Content-Type', 'text/plain');
+            // return res.send({ auth: true, admin,token:token });
         } catch (err) {
             return next(err)
         }
@@ -303,23 +301,22 @@ router.post('/KitchenLogin',CustomerSignInValidations,async (req, res, next) => 
 
 // This Route is for email verfication through email,and will update its status to true 
 // Get - localhost:3001/kitchen/verifyEmail
-router.get('/verifyEmail', async (req, res, next) => {
-
+router.post('/verifyEmail', async (req, res, next) => {
 
     // Check if this user already exisits
-    if (req.query.id !== "") {
+    if (req.body._id !== "") {
         try {
 
-            let admin = await KitchenSignupSchema.findOneAndUpdate({ _id: req.query.id }, { status: true })
+            let admin = await KitchenSignupSchema.findOneAndUpdate({ _id: req.body._id }, { status: true })
 
 
-            res.status(200).send({ auth: true, admin });
+            res.status(200).send({ updated: true, success: admin });
 
         } catch (err) {
             return next(err)
         }
     } else {
-        res.status(200).send({ "error": 'Something is missing.' });
+        res.status(200).send({ "error": 'Something is missing.', updated: false, });
     }
 
 
